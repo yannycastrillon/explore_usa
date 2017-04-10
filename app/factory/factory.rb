f89608e3ba1710dee79b96320d635926c7184efc
Yanny Castrillon <yannyandres117@gmail.com>
2017-04-09 17:05:39 -0700

    Env variable for the API key

--- app/factory/factory.rb
+++ app/factory/factory.rb
@@ -1,6 +1,7 @@
 class Factory
   FIELDS = "title,artist,artist_title,caption,state_province,city,country,date_created,license_model"
-  HEADERS = {"Api-Key" => "j878g39yx378pa77djthzzpn"}
+
+  HEADERS = {"Api-Key" => ENV["Api_Key"]}
 
   def self.find_image(id)
     HTTParty.get("https://api.gettyimages.com/v3/images?ids=#{id}&fields=#{FIELDS}",headers: HEADERS)
@@ -13,4 +14,6 @@ class Factory
   def self.search_creative_images(search_term)
     HTTParty.get("https://api.gettyimages.com/v3/search/images/creative?phrase=#{search_term}",headers:HEADERS)
   end
+
+
 end
